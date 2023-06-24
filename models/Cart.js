3d86538785466be9657885a408031cc30f012621
VinayakSingh2001 <vinayakthakur843@gmail.com>
2023-06-24 16:53:10 +0530

    added new changes

--- models/Cart.js
+++ models/Cart.js
@@ -1,22 +1,21 @@
-const mongoose = require("mongoose")
+const mongoose = require("mongoose");
 
 const CartSchema = new mongoose.Schema(
-    {
-        userId: {type: String , required:true},
-        products: [
-            {
-                productId:{
-                    type:String
-                },
-                quantity:{
-                    type:Number,
-                    default: 1
-                }
-
-            }
-        ],
-    },
-    {timestamps: true}
+  {
+    userId: { type: String, required: true },
+    products: [
+      {
+        productId: {
+          type: String,
+        },
+        quantity: {
+          type: Number,
+          default: 1,
+        },
+      },
+    ],
+  },
+  { timestamps: true }
 );
 
 module.exports = mongoose.model("Cart", CartSchema);
--- routes/auth.js
+++ routes/auth.js
@@ -1,45 +1,57 @@
 const User = require("../models/User");
 const CryptoJS = require("crypto-js");
 const router = require("express").Router();
+const jwt = require("jsonwebtoken");
 
 //REGISTER
-router.post("/register", async (req,res)=>{
-    const newUser = new User({
-        username: req.body.username,
-        email: req.body.email,
-        password: CryptoJS.AES.encrypt(req.body.password, process.env.PASS_SEC).toString(),
+router.post("/register", async (req, res) => {
+  const newUser = new User({
+    username: req.body.username,
+    email: req.body.email,
+    password: CryptoJS.AES.encrypt(
+      req.body.password,
+      process.env.PASS_SEC
+    ).toString(),
+  });
 
-    });
-
-    try{
-
-        const savedUser = await newUser.save();
-        res.status(201).json(savedUser);
-    }catch(err){
-        res.status(500).json(err);
-    }
-
-    
+  try {
+    const savedUser = await newUser.save();
+    res.status(201).json(savedUser);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 //LOGIN
 
-router.post("/login", async (req,res)=>{
-    try{
-        const user = await User.findOne({username: req.body.username});
-        !user && res.status(401).json("Wrong Credentials!");
+router.post("/login", async (req, res) => {
+  try {
+    const user = await User.findOne({ username: req.body.username });
+    !user && res.status(401).json("Wrong Credentials!");
 
-        const hashedPassword = CryptoJS.AES.decrypt(user.password, process.env.PASS_SEC);
+    const hashedPassword = CryptoJS.AES.decrypt(
+      user.password,
+      process.env.PASS_SEC
+    );
 
-        const OriginalPassword = hashedPassword.toString(CryptoJS.enc.Utf8);
-        OriginalPassword !== req.body.password && res.status(401).json("Wrong Credentials!");
+    const OriginalPassword = hashedPassword.toString(CryptoJS.enc.Utf8);
+    OriginalPassword !== req.body.password &&
+      res.status(401).json("Wrong Credentials!");
 
-        const { password , ...others } = user._doc;
+    const accessToken = jwt.sign(
+      {
+        id: user._id,
+        isAdmin: user.isAdmin,
+      },
+      process.env.JWT_SEC,{expiresIn:"3d"}
+    );
 
-        res.status(200).json(others);
-    }catch(err){
-        res.status(500).json(err);
-    }
+    const { password, ...others } = user._doc;
+
+    res.status(200).json({...others, accessToken});
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 module.exports = router;
