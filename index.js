d62be1178ab4dc84c701c798f5523a19872596c3
VinayakSingh2001 <vinayakthakur843@gmail.com>
2023-06-29 17:51:50 +0530

    added new changes

--- index.js
+++ index.js
@@ -1,21 +1,20 @@
-const express = require("express");
+const express = require('express');
 const app = express();
-const mongoose = require("mongoose");
-const dotenv = require("dotenv");
-const userRoute = require("./routes/user")
-const authRoute = require("./routes/auth")
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+const userRoute = require("./routes/user");
+const authRoute = require("./routes/auth");
 
 dotenv.config();
 
-mongoose.connect(process.env.MONGO_URL).then(()=>console.log("DBConnection successfull")).catch((err)=>{console.log(err);
-});
-
-app.use(express.json()); 
-app.use("/api/users",userRoute);
-app.use("/api/auth",authRoute);
 
 
+mongoose.connect(process.env.MONGO_URL).then(()=>console.log("DB Connection successful")).catch((err)=>{console.log(err);});
 
-app.listen(process.env.PORT || 3000, () => {
-    console.log("backend server is running");
-});
+app.use(express.json());
+app.use("/api/auth", authRoute);
+app.use("/api/users", userRoute);
+
+app.listen(process.env.PORT || 3000,()=>{
+    console.log('Backend server is running!');
+})
