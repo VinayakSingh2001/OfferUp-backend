da4c4ab8364d92f1404eb40b1d7fdcf559958e51
VinayakSingh2001 <vinayakthakur843@gmail.com>
2023-06-30 19:35:36 +0530

    changes in cart,order,products,user CRUD operations

--- index.js
+++ index.js
@@ -4,6 +4,10 @@ const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 const userRoute = require("./routes/user");
 const authRoute = require("./routes/auth");
+const productRoute = require("./routes/product");
+const cartRoute = require("./routes/cart");
+const orderRoute = require("./routes/order");
+
 
 dotenv.config();
 
@@ -14,6 +18,10 @@ mongoose.connect(process.env.MONGO_URL).then(()=>console.log("DB Connection succ
 app.use(express.json());
 app.use("/api/auth", authRoute);
 app.use("/api/users", userRoute);
+app.use("/api/products", productRoute);
+app.use("/api/carts", cartRoute);
+app.use("/api/orders", orderRoute);
+
 
 app.listen(process.env.PORT || 3000,()=>{
     console.log('Backend server is running!');
